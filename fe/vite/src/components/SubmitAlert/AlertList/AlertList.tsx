ad913e4f841aa5b0805591195ceab9c9b3bd954c
bsodocean <socajowa3@gmail.com>
2024-12-06 19:45:15 +0100

    Added edit alert modal

--- fe/vite/src/components/SubmitAlert/AlertList/AlertList.tsx
+++ fe/vite/src/components/SubmitAlert/AlertList/AlertList.tsx
@@ -1,24 +1,77 @@
-import { FC, useState } from "react";
-import "./AlertList.css";
+import { FC, useState, useEffect } from "react";
 import SubmitAlert from "../SubmitAlert";
+import "./AlertList.css";
 
 interface Alert {
   id: number;
   name: string;
   age: number;
-  file: string;
+  description: string;
+  file?: string | File | null;
 }
 
 const AlertList: FC = () => {
-  const [alerts, setAlerts] = useState<Alert[]>([]);
+  const [alerts, setAlerts] = useState<Alert[]>([
+    {
+      name: "René Descartes",
+      age: 23,
+      id: 1142002,
+      description: "I think, therefore I am.",
+    },
+    {
+      name: "William Shakespear",
+      age: 30,
+      id: 2002411,
+      description:
+        "Be not afraid of greatness. Some are born great, some achieve greatness, and others have greatness thrust upon them.",
+    },
+  ]);
+
+  const [activeAlert, setActiveAlert] = useState<Alert | null>(null);
+  const [activeEdit, setActiveEdit] = useState(false);
+  const [newFormData, setNewFormData] = useState({
+    name: "",
+    age: "",
+    description: "",
+    file: null,
+  });
+
+  useEffect(() => {
+    if (activeAlert) {
+      setNewFormData({
+        name: activeAlert.name,
+        age: String(activeAlert.age),
+        description: activeAlert.description,
+        file: activeAlert.file ?? null, // ! This works, but the compiler has an issue with it ! //
+      });
+    }
+  }, [activeAlert]);
+
+  const toggleDetails = (id: number) => {
+    const selectedAlert = alerts.find((alert) => alert.id === id);
+    setActiveAlert((prevState) =>
+      prevState?.id === id ? null : selectedAlert || null
+    );
+  };
+
+  const closeModal = () => {
+    setActiveAlert(null);
+  };
 
   const addAlertEntry = (alert: Omit<Alert, "id">) => {
-    const newAlert = { ...alert, id: Date.now() }; //
+    const newAlert = { ...alert, id: Date.now() };
     setAlerts((prevAlerts) => [...prevAlerts, newAlert]);
   };
 
-  const editEntry = (entryId: number) => {
-    // Implementation for editing an alert
+  const editEntry = () => {
+    setActiveEdit((prevState) => !prevState);
+    if (activeAlert) {
+      setActiveAlert({
+        ...activeAlert,
+        ...newFormData,
+        age: Number(newFormData.age),
+      });
+    }
   };
 
   const deleteEntry = (entryId: number) => {
@@ -31,32 +84,140 @@ const AlertList: FC = () => {
     <div className='alert-table-container'>
       <h2>Alert List</h2>
       <SubmitAlert addAlertEntry={addAlertEntry} />
-      <table className='alert-table'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>File</th>
-          </tr>
-        </thead>
-        <tbody>
-          {alerts.map((alert) => (
-            <tr key={alert.id}>
-              <td>{alert.id}</td>
-              <td>{alert.name}</td>
-              <td>{alert.age}</td>
-              <td>
-                {alert.file ? <a href={alert.file}>Download</a> : "No file"}
-              </td>
-              <td>
-                <button onClick={() => editEntry(alert.id)}>Edit</button>
-                <button onClick={() => deleteEntry(alert.id)}>Delete</button>
-              </td>
+      {alerts.length > 0 ? (
+        <table className='alert-table'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>File</th>
+              <th>Details</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {alerts.map((alert) => (
+              <tr key={alert.id}>
+                <td>{alert.id}</td>
+                <td>{alert.name}</td>
+                <td>{alert.age}</td>
+                <td>
+                  {alert.file ? (
+                    <a
+                      href={
+                        typeof alert.file === "string"
+                          ? alert.file
+                          : URL.createObjectURL(alert.file)
+                      }
+                    >
+                      Download
+                    </a>
+                  ) : (
+                    "No file"
+                  )}
+                </td>
+                <td>
+                  <button onClick={() => toggleDetails(alert.id)}>
+                    {activeAlert?.id === alert.id
+                      ? "Hide Details"
+                      : "Show Details"}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>You're up to date!</p>
+      )}
+
+      {activeAlert && activeAlert && (
+        <div className='modal-overlay' onClick={closeModal}>
+          <div className='modal' onClick={(e) => e.stopPropagation()}>
+            <button className='close-button' onClick={closeModal}>
+              X
+            </button>
+            {activeEdit ? (
+              <>
+                <h2>Details for Alert {activeAlert.id}</h2>
+                <strong>Name:</strong>
+                <input
+                  type='text'
+                  value={newFormData.name}
+                  onChange={(e) =>
+                    setNewFormData({ ...newFormData, name: e.target.value })
+                  }
+                />
+                <strong>Age:</strong>
+                <input
+                  type='text'
+                  value={newFormData.age}
+                  onChange={(e) =>
+                    setNewFormData({ ...newFormData, age: e.target.value })
+                  }
+                />
+                <strong>Description:</strong>
+                <input
+                  type='text'
+                  value={newFormData.description}
+                  onChange={(e) =>
+                    setNewFormData({
+                      ...newFormData,
+                      description: e.target.value,
+                    })
+                  }
+                />
+                <strong>File:</strong>
+                {newFormData.file ? (
+                  <a href={URL.createObjectURL(newFormData.file)}>Download</a>
+                ) : (
+                  "No file"
+                )}
+              </>
+            ) : (
+              <>
+                <h2>Details for Alert {activeAlert.id}</h2>
+                <p>
+                  <strong>Name:</strong> {activeAlert.name}
+                </p>
+                <p>
+                  <strong>Age:</strong> {activeAlert.age}
+                </p>
+                <p>
+                  <strong>Description:</strong> {activeAlert.description}
+                </p>
+                <p>
+                  <strong>File:</strong>{" "}
+                  {activeAlert.file ? (
+                    <a
+                      href={
+                        typeof activeAlert.file === "string"
+                          ? activeAlert.file
+                          : activeAlert.file
+                          ? URL.createObjectURL(activeAlert.file)
+                          : "#"
+                      }
+                    >
+                      Download
+                    </a>
+                  ) : (
+                    "No file"
+                  )}
+                </p>
+              </>
+            )}
+            <button className='edit-btn' onClick={editEntry}>
+              Edit
+            </button>
+            <button
+              className='delete-btn'
+              onClick={() => deleteEntry(activeAlert.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- fe/vite/src/components/SubmitAlert/SubmitAlert.tsx
+++ fe/vite/src/components/SubmitAlert/SubmitAlert.tsx
@@ -2,33 +2,51 @@ import { FC, useState } from "react";
 import "./SubmitAlert.css";
 
 interface SubmitAlertProps {
-  addAlertEntry: (alert: { name: string; age: number; file: string }) => void;
+  addAlertEntry: (alert: {
+    name: string;
+    age: number;
+    description: string;
+    file: string;
+  }) => void;
 }
 
 const SubmitAlert: FC<SubmitAlertProps> = ({ addAlertEntry }) => {
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<{
+    name: string;
+    age: string;
+    description: string;
+    file: File | null;
+  }>({
     name: "",
     age: "",
-    file: null as File | null,
+    description: "",
+    file: null,
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files ? e.target.files[0] : null;
-    setFormData({ ...formData, file });
+    setFormData((prev) => ({ ...prev, file }));
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const age = parseInt(formData.age, 10);
-    if (isNaN(age)) return; // Guard against invalid age input
+    if (isNaN(age)) return;
     const fileName = formData.file ? formData.file.name : "No file";
-    addAlertEntry({ name: formData.name, age, file: fileName });
-    setFormData({ name: "", age: "", file: null }); // Reset form after submission
+    addAlertEntry({
+      name: formData.name,
+      age,
+      description: formData.description,
+      file: fileName,
+    });
+    setFormData({ name: "", age: "", description: "", file: null });
   };
 
   return (
@@ -43,6 +61,7 @@ const SubmitAlert: FC<SubmitAlertProps> = ({ addAlertEntry }) => {
             value={formData.name}
             onChange={handleInputChange}
             required
+            placeholder='Enter your name'
           />
         </div>
         <div className='form-field'>
@@ -54,6 +73,7 @@ const SubmitAlert: FC<SubmitAlertProps> = ({ addAlertEntry }) => {
             value={formData.age}
             onChange={handleInputChange}
             required
+            placeholder='Enter your age'
           />
         </div>
         <div className='form-field'>
@@ -63,6 +83,19 @@ const SubmitAlert: FC<SubmitAlertProps> = ({ addAlertEntry }) => {
             id='file'
             name='file'
             onChange={handleFileChange}
+          />
+        </div>
+        <div className='form-field'>
+          <label htmlFor='description'>Description</label>
+          <textarea
+            id='description'
+            name='description'
+            value={formData.description}
+            onChange={handleInputChange}
+            required
+            placeholder='Enter a detailed description...'
+            rows={4}
+            cols={40}
           />
         </div>
         <button type='submit'>Submit</button>
